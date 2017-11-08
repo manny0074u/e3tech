04237b3e5070cfab8d0e9ca7b68186ea970f91f2
manny0074u <aboajahemmanuel@gmail.com>
2017-11-08 07:03:08 +0100

    update

--- inc/header.php
+++ inc/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Stack Multipurpose HTML Template</title>
+        <title>E3 Technologies</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Site Description Here">
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
@@ -55,22 +55,22 @@
                             <div class="bar__module">
                                 <ul class="menu-horizontal text-left">
                                     <li>
-                                        <a href="index.html">Home</a>
+                                        <a href="index.php">Home</a>
                                     </li>
                                     <li>
-                                        <a href="about.html">About</a>   
+                                        <a href="about.php">About</a>   
                                     </li>
                                     <li>
-                                        <a href="services.html">Services</a>
+                                        <a href="services.php">Services</a>
                                     </li>
                                     <li>
-                                        <a href="portfolio.html">Portfolio</a>   
+                                        <a href="portfolio.php">Portfolio</a>   
                                     </li>
-                                    <li>
+                                    <!--<li>
                                         <a href="blog.html">Blog</a>
-                                    </li>
+                                    </li>-->
                                     <li>
-                                        <a href="contact.html">Contact</a>   
+                                        <a href="contact.php">Contact</a>   
                                     </li>
                                 </ul>
                             </div>
